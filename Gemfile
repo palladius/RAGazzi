59229560131f2bfd27531b7b9efe22b522c6d01a
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-06-17 11:58:50 +0200

    first thing

---
+++ Gemfile
@@ -0,0 +1,8 @@
+
+source "https://rubygems.org"
+
+gem "langchainrb", "~> 0.13.1"
+gem 'nano-bots', '~> 3.1'
+
+# TODO investigate this gem too
+# gem 'gen-ai', '~> 0.4.3'
---
+++ main.rb
@@ -0,0 +1,6 @@
+
+puts("TODO import nanobots and langchainrb and instantiate Gemini")
+
+# inspired by https://github.com/patterns-ai-core/ecommerce-ai-assistant-demo/blob/main/main.rb
+require "irb"
+IRB.start(__FILE__)
