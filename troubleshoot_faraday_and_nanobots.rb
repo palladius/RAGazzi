747b1818f91129d2987cdb933c554c08c0eca1b1
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-06-18 08:22:34 +0200

    still broken

--- troubleshoot_faraday_and_nanobots.rb
+++ troubleshoot_faraday_and_nanobots.rb
@@ -1,26 +1,46 @@
 #!/usr/bin/env ruby
+=begin
+  I still get a 400. Seems like 400 means API key not valid.
+  https://stackoverflow.com/questions/77681343/googlegenerativeaierror-400-bad-request-api-key-not-valid-please-pass-a-vali
+  But my key is valid since I use it as it is in the curl_test_api_key.sh and it works!!!
+
+=end
 
 require 'nano-bots'
 require 'faraday'
 require 'pp'
 require 'pry'
 
+# WOW: Healthy monjkeypatching through prepend.
+
 module Faraday
   # Response represents an HTTP response from making an HTTP request.
+  # UGLY monkeypatching
   class Response
-    extend Forwardable
-    extend MiddlewareRegistry
+    #extend Forwardable
+    #extend MiddlewareRegistry
 
     def on_complete(&block)
       verbose = false
-      puts("🙈 Riccardo nice monkeypatch. to_hash=#{to_hash}")
-      puts("🙈 Riccardo nice monkeypatch. 1. Request=#{env.request_body}")
-      puts("🙈 Riccardo nice monkeypatch. 2. Response Headers=#{env.response_headers}")
-      puts("🙈 Riccardo nice monkeypatch. 2. Response Body=#{env.response_body}")
-      puts("🙈 Riccardo nice monkeypatch. env.keys=#{env.keys}")
-      puts("🙈 Riccardo nice monkeypatch. env=#{env}") if verbose
-      nice_env = Pry::ColorPrinter.pp(env)
-      File.write('t.faraday.txt', nice_env, mode: 'w')
+      puts("🙉 Riccardo monkeypatch Faraday::Response on_complete()")
+
+      puts("  🙈 on_complete() to_hash=#{to_hash}")
+      puts("  🙈 on_complete() url=#{env.url}")
+      puts("  🙈 on_complete() => Request Body=#{env.request_body}")
+      puts("  🙈 on_complete() => Request Headers=#{env.request_headers}")
+      puts("  🙈 on_complete() <= Response Headers=#{env.response_headers}")
+      puts("  🙈 on_complete() <= Response Body=#{env.response_body}")
+      puts("  🙈 on_complete() env.keys=#{env.keys}")
+      puts("  🙈 on_complete() env=#{env}") if verbose
+      #nice_env = Pry::ColorPrinter.pp(env)
+      #File.write('t.faraday.txt', nice_env, mode: 'w')
+      File.open('t.faraday.txt', mode: 'w') do |f|
+        PP.pp(env, f)
+      end
+      File.open('t.faraday_color.txt', mode: 'w') do |f|
+        Pry::ColorPrinter.pp(env, f)
+      end
+
  #     File.write('/path/to/file', 'Some glorious content', mode: 'a')
 
       if finished?
@@ -31,6 +51,30 @@ module Faraday
       end
       self
     end
+  end
+  # from connection.rb
+
+
+    # PRETTY monkeypatching through super and prepend.
+  module FaradayConnectionExtensions
+    def run_request(method, url, body, headers)
+      puts("🙉 Riccardo monkeypatch Faraday::Connection run_request")
+      puts("  🙉 run_request() method=#{method}")
+      puts("  🙉 run_request() url=#{url}")
+      puts("  🙉 run_request() body=#{body}")
+      puts("  🙉 run_request() headers=#{headers}")
+      puts("🙉 run_request() Hmmm seems a lot of empty stuff, I need to dig deeper")
+      super(method, url, body, headers)
+    end
+  end
+
+
+  class Connection
+    prepend FaradayConnectionExtensions
+    # https://stackoverflow.com/questions/4470108/when-monkey-patching-an-instance-method-can-you-call-the-overridden-method-from
+
+    # def run_request(method, url, body, headers)
+    # end
   end
 end
 
